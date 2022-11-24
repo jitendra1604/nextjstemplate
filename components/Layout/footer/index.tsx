e79713f6f69bbb54ede3df366b68c86f93a3c471
ujame <107609459+Ujames0707@users.noreply.github.com>
2022-11-24 16:07:39 +0530

    adding footer data

--- components/Layout/footer/index.tsx
+++ components/Layout/footer/index.tsx
@@ -1,19 +1,96 @@
 import React from "react";
 import Borderimg from "../../../public/images/footerborder.svg";
 import Image from "next/image";
-
+import { Footerdata } from "../../Data/footer";
+import styles from "../../../styles/components/Footer.module.scss";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 export const Footer = () => {
   return (
-    <section>
+    <section className={`${styles.footer_scetion} container`}>
       <div>
-        <Image src={Borderimg} alt="" />
-        <p>dkkddjkj</p>
-        <a href="#" className="btn btn-violet">
-          Green Button
-        </a>
-        <a href="#" className="btn-border btn-white">
-          Green Button
-        </a>
+        {Footerdata.map((main, index) => {
+          return (
+            <>
+              <div>
+                <Image className={styles.imageborder} src={Borderimg} alt="" />
+                <h3>{main.title}</h3>
+                <ul className={styles.sublinks}>
+                  {main.subtitle.map((sub) => {
+                    return (
+                      <li>
+                        <a href="#">
+                          <h2>{sub.name}</h2>
+                        </a>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
+              <Row>
+                <Col>
+                  <p>{main.content}</p>
+                  <div className={styles.mailsection}>
+                    <div>
+                      <Image src={main.Image} width={24} height={24} alt="" />
+                    </div>
+                    <div>
+                      <p>{main.mail}</p>
+                    </div>
+                  </div>
+                </Col>
+                <Col>
+                  <div>
+                    <Row>
+                      <Col>
+                        {main.links.map((dataname, index) => {
+                          return (
+                            <div>
+                              <p>{dataname.title}</p>
+                              <ul>
+                                {dataname.sublinks?.map((links, index) => {
+                                  return (
+                                    <li>
+                                      <a href="#">{links.name}</a>
+                                    </li>
+                                  );
+                                })}
+                              </ul>
+                            </div>
+                          );
+                        })}
+                      </Col>
+                      <Col>
+                        {main.links.map((dataname, index) => {
+                          return (
+                            <div>
+                              {dataname.linkssecond?.map((links, index) => {
+                                return (
+                                  <>
+                                    <p>{links.title}</p>
+                                    <ul>
+                                      <li>
+                                        <a href="#">
+                                          {links.sublinks.map((links) => {
+                                            return <>{links.name}</>;
+                                          })}
+                                        </a>
+                                      </li>
+                                    </ul>
+                                  </>
+                                );
+                              })}
+                            </div>
+                          );
+                        })}
+                      </Col>
+                    </Row>
+                  </div>
+                </Col>
+              </Row>
+            </>
+          );
+        })}
       </div>
     </section>
   );
