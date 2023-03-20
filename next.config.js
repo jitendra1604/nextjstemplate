e695529e1a3d60c07e604f2a30d24e3906832eeb
jeet <jitendra.suthar@terralogic.com>
2023-03-20 15:40:10 +0530

    features added

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
+const { ANALYZE } = process.env;
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: ANALYZE === "true",
+});
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 };
 
-module.exports = nextConfig;
+module.exports = withBundleAnalyzer(nextConfig);
